09a938bae512c0d07d8f29b4d10a173118598524
Raphael Feitosa <raphaelcs2@gmail.com>
2021-05-10 17:31:33 -0300

    first commit

---
+++ src/main/java/com/raphaelfeitosa/springbootionic/SpringbootionicApplication.java
@@ -0,0 +1,13 @@
+package com.raphaelfeitosa.springbootionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootionicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootionicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raphaelfeitosa/springbootionic/SpringbootionicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphaelfeitosa.springbootionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootionicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
