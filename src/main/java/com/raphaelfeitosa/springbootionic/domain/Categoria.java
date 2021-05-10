7372a270d849b94fecf0ed2cf1adf35e74a8a88a
Raphael Feitosa <raphaelcs2@gmail.com>
2021-05-10 18:14:15 -0300

    test class domain

---
+++ src/main/java/com/raphaelfeitosa/springbootionic/domain/Categoria.java
@@ -0,0 +1,49 @@
+package com.raphaelfeitosa.springbootionic.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String nome;
+
+    public  Categoria () {
+    }
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria)) return false;
+
+        Categoria categoria = (Categoria) o;
+
+        return getId().equals(categoria.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getId().hashCode();
+    }
+}
--- src/main/java/com/raphaelfeitosa/springbootionic/resources/CategoriaResource.java
+++ src/main/java/com/raphaelfeitosa/springbootionic/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.raphaelfeitosa.springbootionic.resources;
 
+import com.raphaelfeitosa.springbootionic.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "REST está funcionando";
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1, "Informatica");
+        Categoria cat2 = new Categoria(2, "Escritorio");
+
+        List<Categoria> list = new ArrayList<>();
+
+        list.add(cat1);
+        list.add(cat2);
+
+        return list;
     }
 
 }
