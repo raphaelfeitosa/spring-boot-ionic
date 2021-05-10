7d77e2470eef2c4a59b86ec5313de7f657f35928
Raphael Feitosa <raphaelcs2@gmail.com>
2021-05-10 19:55:33 -0300

    create operation instanciation

--- src/main/java/com/raphaelfeitosa/springbootionic/SpringbootionicApplication.java
+++ src/main/java/com/raphaelfeitosa/springbootionic/SpringbootionicApplication.java
@@ -1,13 +1,31 @@
 package com.raphaelfeitosa.springbootionic;
 
+import com.raphaelfeitosa.springbootionic.domain.Categoria;
+import com.raphaelfeitosa.springbootionic.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringbootionicApplication {
+public class SpringbootionicApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootionicApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+
+        Categoria cat1 = new Categoria(null, "Informatica");
+        Categoria cat2 = new Categoria(null, "Escritorio");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
