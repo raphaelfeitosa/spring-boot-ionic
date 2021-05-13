a08748aef451d90a3e437547767022a46a86cc1c
Raphael Feitosa <raphaelcs2@gmail.com>
2021-05-12 21:30:54 -0300

    PUT, DELETE and GET Cliente

--- src/main/java/com/raphaelfeitosa/springbootionic/resources/ClienteResource.java
+++ src/main/java/com/raphaelfeitosa/springbootionic/resources/ClienteResource.java
@@ -1,13 +1,18 @@
 package com.raphaelfeitosa.springbootionic.resources;
 
 import com.raphaelfeitosa.springbootionic.domain.Cliente;
+import com.raphaelfeitosa.springbootionic.dto.ClienteDTO;
 import com.raphaelfeitosa.springbootionic.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/clientes")
@@ -18,10 +23,50 @@ public class ClienteResource {
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Cliente> find(@PathVariable Long id){
-
         Cliente cliente = clienteService.find(id);
         return ResponseEntity.ok().body(cliente);
-
     }
+
+//    @RequestMapping(method = RequestMethod.POST)
+//    public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO clienteDTO){
+//        Cliente cliente = clienteService.fromDTO(clienteDTO);
+//        cliente = clienteService.insert(cliente);
+//        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+//                .path("/{id}").buildAndExpand(cliente.getId()).toUri();
+//        return ResponseEntity.created(uri).build();
+//    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO clienteDTO, @PathVariable Long id){
+        Cliente cliente = clienteService.fromDTO(clienteDTO);
+        cliente.setId(id);
+        clienteService.update(cliente);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        clienteService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<ClienteDTO>> findAll(){
+        List<Cliente> listcliente = clienteService.findAll();
+        List<ClienteDTO> listclienteDTO = listcliente.stream().map(listDTO-> new ClienteDTO(listDTO)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listclienteDTO);
+    }
+
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    public ResponseEntity<Page<ClienteDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction){
+        Page<Cliente> listcliente = clienteService.findPage(page, linesPerPage, orderBy, direction);
+        Page<ClienteDTO> listclienteDTO = listcliente.map(listDTO-> new ClienteDTO(listDTO));
+        return ResponseEntity.ok().body(listclienteDTO);
+    }
+
 
 }
--- src/main/java/com/raphaelfeitosa/springbootionic/services/ClienteService.java
+++ src/main/java/com/raphaelfeitosa/springbootionic/services/ClienteService.java
@@ -1,11 +1,18 @@
 package com.raphaelfeitosa.springbootionic.services;
 
 import com.raphaelfeitosa.springbootionic.domain.Cliente;
+import com.raphaelfeitosa.springbootionic.dto.ClienteDTO;
 import com.raphaelfeitosa.springbootionic.repositories.ClienteRepository;
+import com.raphaelfeitosa.springbootionic.services.exceptions.DataIntegrityException;
 import com.raphaelfeitosa.springbootionic.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,4 +26,39 @@ public class ClienteService {
         return cliente.orElseThrow(() -> new ObjectNotFoundException(
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
     }
+
+    public Cliente update(Cliente cliente) {
+        Cliente newCliente = find(cliente.getId());
+        updateData(newCliente, cliente);
+        return clienteRepository.save(newCliente);
+    }
+
+    private void updateData(Cliente newCliente, Cliente cliente) {
+        newCliente.setNome(cliente.getNome());
+        newCliente.setEmail(cliente.getEmail());
+    }
+
+    public void delete(Long id) {
+        find(id);
+        try {
+            clienteRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir porque há entidades relacionados");
+        }
+    }
+
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+    //Paginação de cliente
+    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return clienteRepository.findAll(pageRequest);
+    }
+    public Cliente fromDTO(ClienteDTO clienteDTO){
+        return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+    }
+
+//    public Cliente insert(Cliente cliente) {
+//    }
 }
