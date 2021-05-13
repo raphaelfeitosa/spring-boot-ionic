8dd509d107a64b31b05c851927071e4938c0d040
Raphael Feitosa <raphaelcs2@gmail.com>
2021-05-13 13:50:48 -0300

    email validation already registered

--- src/main/java/com/raphaelfeitosa/springbootionic/repositories/ClienteRepository.java
+++ src/main/java/com/raphaelfeitosa/springbootionic/repositories/ClienteRepository.java
@@ -2,6 +2,10 @@ package com.raphaelfeitosa.springbootionic.repositories;
 
 import com.raphaelfeitosa.springbootionic.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email);
 }
--- src/main/java/com/raphaelfeitosa/springbootionic/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/raphaelfeitosa/springbootionic/services/validation/ClienteInsertValidator.java
@@ -1,15 +1,21 @@
 package com.raphaelfeitosa.springbootionic.services.validation;
 
+import com.raphaelfeitosa.springbootionic.domain.Cliente;
 import com.raphaelfeitosa.springbootionic.dto.ClienteNewDTO;
 import com.raphaelfeitosa.springbootionic.enums.TipoCliente;
+import com.raphaelfeitosa.springbootionic.repositories.ClienteRepository;
 import com.raphaelfeitosa.springbootionic.resources.exceptions.FieldMessage;
 import com.raphaelfeitosa.springbootionic.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.ArrayList;
 import java.util.List;
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+    @Autowired
+    private ClienteRepository clienteRepository;
+
     @Override
     public void initialize(ClienteInsert ann) {
     }
@@ -22,6 +28,13 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
         }
         if(clienteNewDTO.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(clienteNewDTO.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        Cliente aux = clienteRepository.findByEmail(clienteNewDTO.getEmail());
+
+        if (aux != null) {
+
+            list.add(new FieldMessage("email", "Email já cadastrado"));
         }
 
         for (FieldMessage e : list) {
